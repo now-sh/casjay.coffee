5dad3821112692bb25c66fd7be40cd42d25c1820
casjay <git-admin@casjaysdev.com>
2023-08-10 20:22:13 -0400

    🗃️ Updated domain names 🗃️
    LICENSE.md
    src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -58,11 +58,11 @@
       <!-- Begin Casjays Developments Footer -->
       Copyright 1999 - 2023
       <br />
-      <a href="https://casjay.net" target="_blank">Casjays Developments</a> and
+      <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and
       <a href="https://malaks-us.github.io/jason" target="_blank">Jason M. Hempstead-Malak</a><br />
       Hosting by
-      <a href="https://casjay.net/hosting" target="_blank">Casjays Developments: Hosting</a><br />
-      and powered by <a href="https://casjaysdev.com" target="_blank">CasjaysDev.com</a>
+      <a href="https://casjay.pro/hosting" target="_blank">Casjays Developments: Hosting</a><br />
+      and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev.com</a>
       <br />
       <br />
       <a href="https://www.patreon.com/casjay" target="_blank"
@@ -78,16 +78,16 @@
       /></a>
       <br />
       <br />
-      <a href="https://help.casjay.net" target="_blank">CasjaysDev Support</a> |
-      <a href="https://bugs.casjay.net" target="_blank">CasjaysDev Bugs</a> <br />
-      <a href="https://status.casjay.net" target="_blank">System Status</a> |
-      <a href="https://servers.casjay.net" target="_blank">Service Status</a> |
-      <a href="https://status.casjaysdev.com" target="_blank">Complete Status</a> <br />
-      <a href="https://casjaysdev.com/domains.html" target="_blank">Casjays Developments Domains</a>
+      <a href="https://help.casjay.pro" target="_blank">CasjaysDev Support</a> |
+      <a href="https://bugs.casjay.pro" target="_blank">CasjaysDev Bugs</a> <br />
+      <a href="https://status.casjay.pro" target="_blank">System Status</a> |
+      <a href="https://servers.casjay.pro" target="_blank">Service Status</a> |
+      <a href="https://status.casjaysdev.pro" target="_blank">Complete Status</a> <br />
+      <a href="https://casjaysdev.pro/domains.html" target="_blank">Casjays Developments Domains</a>
       <br />
       <br />
       Casjays Developments privacy policy can be found at<br />
-      <a href="https://casjaysdev.com/policy/" target="_blank">https://casjaysdev.com/policy</a>
+      <a href="https://casjaysdev.pro/policy/" target="_blank">https://casjaysdev.pro/policy</a>
       <br />
       <!-- End Casjays Developments Footer -->
       <!-- Begin theme Code -->
