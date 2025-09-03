d98cabce59188f535732ad60147fd2d415d4cc7e
casjay <git-admin@casjaysdev.pro>
2025-09-02 22:23:56 -0400

    🗃️ Modified: src/App.vue 🗃️
    Modified: src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       type="button"
       style="width: 100px; color: #ffffff"
       class="m-2 text-left justify-content-between btn btn-outline-danger"
-      ><img src="/logo.png" height="64" width="64" alt="Home" />
+    ><img src="/logo.png" height="64" width="64" alt="Home" />
     </router-link>
     <router-link
       to="/Projects"
@@ -58,47 +58,53 @@
       <!-- Begin Casjays Developments Footer -->
       Copyright 1999 - 2024
       <br />
-      <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and
-      <a href="https://malaks-us.github.io/jason" target="_blank">Jason M. Hempstead-Malak</a><br />
+      <a href="https://casjay.pro" target="_blank" rel="noopener noreferrer">Casjays Developments</a> and
+      <a href="https://malaks-us.github.io/jason" target="_blank" rel="noopener noreferrer">Jason M. Hempstead-Malak</a><br />
       Hosting by
-      <a href="https://casjay.pro/hosting" target="_blank">Casjays Developments: Hosting</a><br />
-      and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev</a>
+      <a href="https://casjay.pro/hosting" target="_blank" rel="noopener noreferrer">Casjays Developments: Hosting</a><br />
+      and powered by <a href="https://casjaysdev.pro" target="_blank" rel="noopener noreferrer">CasjaysDev</a>
       <br />
       <br />
-      <a href="https://www.patreon.com/casjay" target="_blank"
-        ><img
-          src="https://img.shields.io/badge/patreon-donate-orange.svg"
-          border="0"
-          alt="Casjays Patreon Page"
+      <a
+        href="https://www.patreon.com/casjay"
+        target="_blank"
+        rel="noopener noreferrer"
+      ><img
+        src="https://img.shields.io/badge/patreon-donate-orange.svg"
+        border="0"
+        alt="Casjays Patreon Page"
       /></a>
-      <a href="https://www.paypal.me/casjaysdev" target="_blank"
-        ><img
-          src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com"
-          alt="Casjays Paypal Page"
+      <a
+        href="https://www.paypal.me/casjaysdev"
+        target="_blank"
+        rel="noopener noreferrer"
+      ><img
+        src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com"
+        alt="Casjays Paypal Page"
       /></a>
       <br />
       <br />
-      <a href="https://help.casjay.pro" target="_blank">CasjaysDev Support</a> |
-      <a href="https://bugs.casjay.pro" target="_blank">CasjaysDev Bugs</a> <br />
-      <a href="https://uptime.servicestat.us" target="_blank">System Status</a> |
-      <a href="https://services.servicestat.us" target="_blank">Service Status</a> |
-      <a href="https://uptime.servicestat.us" target="_blank">Complete Status</a> <br />
-      <a href="https://casjaysdev.pro/domains.html" target="_blank">Casjays Developments Domains</a>
+      <a href="https://help.casjay.pro" target="_blank" rel="noopener noreferrer">CasjaysDev Support</a> |
+      <a href="https://bugs.casjay.pro" target="_blank" rel="noopener noreferrer">CasjaysDev Bugs</a> <br />
+      <a href="https://uptime.servicestat.us" target="_blank" rel="noopener noreferrer">System Status</a> |
+      <a href="https://services.servicestat.us" target="_blank" rel="noopener noreferrer">Service Status</a> |
+      <a href="https://uptime.servicestat.us" target="_blank" rel="noopener noreferrer">Complete Status</a> <br />
+      <a href="https://casjaysdev.pro/domains.html" target="_blank" rel="noopener noreferrer">Casjays Developments Domains</a>
       <br />
       <br />
       Casjays Developments privacy policy can be found at<br />
-      <a href="https://casjaysdev.pro/policy/" target="_blank">https://casjaysdev.pro/policy</a>
+      <a href="https://casjaysdev.pro/policy/" target="_blank" rel="noopener noreferrer">https://casjaysdev.pro/policy</a>
       <br />
       <!-- End Casjays Developments Footer -->
       <!-- Begin theme Code -->
       <div class="text-center" style="color: royalblue">
         <br />
         Made with 💜💜💜💜💜💜 by <br />
-        <a href="http://github.com/casjay" target="_blank" rel="noopener">
+        <a href="http://github.com/casjay" target="_blank" rel="noopener noreferrer">
           Jason M. Hempstead (Casjay)</a
         ><br />
         This site is powered by an
-        <a href="https://github.com/casjay-templates" target="_blank"> open source </a>theme
+        <a href="https://github.com/casjay-templates" target="_blank" rel="noopener noreferrer"> open source </a>theme
       </div>
       <br />
       <!-- End Theme powered -->
