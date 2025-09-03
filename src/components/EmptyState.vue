e07cdacdf9dd48d654dba37c35b8238c5a6c712d
casjay <git-admin@casjaysdev.pro>
2025-09-02 22:23:57 -0400

    🗃️ Modified: src/components/EmptyState.vue 🗃️
    Modified: src/components/EmptyState.vue

---
+++ src/components/EmptyState.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <div class="alert alert-warning m-5">
+      <h3>{{ title }}</h3>
+      <p>{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  message: string;
+}
+
+defineProps<Props>();
+</script>
