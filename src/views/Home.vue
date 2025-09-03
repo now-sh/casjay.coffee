fef1804d4a2460f502b98c38349721a38bbbf82a
casjay <git-admin@casjaysdev.pro>
2025-09-02 22:24:08 -0400

    🗃️ Modified: src/views/Home.vue 🗃️
    Modified: src/views/Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import Welcome from '@/components/Welcome.vue'; // @ is an alias to /src
-
-@Options({
-  props: {
-    msg: { type: String },
-    msgSpinner: { type: String },
-  },
-  components: {
-    Welcome,
-  },
-})
-export default class Home extends Vue {}
+<script setup lang="ts">
+import Welcome from '@/components/Welcome.vue';
 </script>
